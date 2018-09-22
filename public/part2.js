4367518e10073360091ab9cb2d7f132067f0c279
Ben Wexler <mixedywex@gmail.com>
2018-09-22 10:54:43 -0400

    Beat repeat off now stops all sounds currently playing

--- public/part2.js
+++ public/part2.js
@@ -46,6 +46,72 @@ function beatRepeatToggle() {
 		beatRepeatBtn.style.backgroundColor = "white";
 		beatRepeatBtn.innerText = "Beat Repeat Off";
 		noteRepeatSwitcher = "off";
+
+		if (counterObj.newPadCounter1>0) {
+			bufferNode1.stop(context.currentTime);
+			counterObj.newPadCounter1 = 0;	
+		}
+		if (counterObj.newPadCounter2>0) {
+			bufferNode2.stop(context.currentTime);
+			counterObj.newPadCounter2 = 0;	
+		}
+		if (counterObj.newPadCounter3>0) {
+			bufferNode3.stop(context.currentTime);
+			counterObj.newPadCounter3 = 0;	
+		}
+		if (counterObj.newPadCounter4>0) {
+			bufferNode4.stop(context.currentTime);
+			counterObj.newPadCounter4 = 0;	
+		}
+		if (counterObj.newPadCounter5>0) {
+			bufferNode5.stop(context.currentTime);
+			counterObj.newPadCounter5 = 0;	
+		}
+		if (counterObj.newPadCounter6>0) {
+			bufferNode6.stop(context.currentTime);
+			counterObj.newPadCounter6 = 0;	
+		}
+		if (counterObj.newPadCounter7>0) {
+			bufferNode7.stop(context.currentTime);
+			counterObj.newPadCounter7 = 0;	
+		}
+		if (counterObj.newPadCounter8>0) {
+			bufferNode8.stop(context.currentTime);
+			counterObj.newPadCounter8 = 0;	
+		}
+		if (counterObj.newPadCounter9>0) {
+			bufferNode9.stop(context.currentTime);
+			counterObj.newPadCounter9 = 0;	
+		}
+		if (counterObj.newPadCounter10>0) {
+			bufferNode10.stop(context.currentTime);
+			counterObj.newPadCounter10 = 0;	
+		}
+		if (counterObj.newPadCounter11>0) {
+			bufferNode11.stop(context.currentTime);
+			counterObj.newPadCounter11 = 0;	
+		}
+		if (counterObj.newPadCounter12>0) {
+			bufferNode12.stop(context.currentTime);
+			counterObj.newPadCounter12 = 0;	
+		}
+		if (counterObj.newPadCounter13>0) {
+			bufferNode13.stop(context.currentTime);
+			counterObj.newPadCounter13 = 0;	
+		}
+		if (counterObj.newPadCounter14>0) {
+			bufferNode14.stop(context.currentTime);
+			counterObj.newPadCounter14 = 0;	
+		}
+		if (counterObj.newPadCounter15>0) {
+			bufferNode15.stop(context.currentTime);
+			counterObj.newPadCounter15 = 0;	
+		}
+		if (counterObj.newPadCounter16>0) {
+			bufferNode16.stop(context.currentTime);
+			counterObj.newPadCounter16 = 0;	
+		}
+		
 	} else {
 		beatRepeatBtn.style.backgroundColor = "yellow";
 		beatRepeatBtn.innerText = "Beat Repeat On";
@@ -96,9 +162,6 @@ function repeatPadEqualsZero(padNumber) {
 for(var i=1; i<=16; i++) {
 		window['repeatPad'+i] = 0;
 	}
-
-let tempo = 140;
-let noteDivision = 8;
 
 let counterObj = {	
 };
